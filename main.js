1a3672d7e9fccdaeec7baad2a45fa0282a053a6d
DamianMilecki <damian.milecki@gmail.com>
2019-09-24 15:14:35 +0200

    add new ball and clean code

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
     
 const makeShot = function (){
     const myContent =document.getElementById('bcontent'); 
-    const myEmoti = ['⚽', '⚾','🥎','🏀','🏐','🏈','🏉']; 
+    const myEmoti = ['⚽', '⚾','🥎','🏀','🏐','🏈','🏉','🎱','💣','☢️']; 
     const myBox = document.getElementById('mbox');
     const newBox = document.createElement('span');
    
-    // myButton.classList.add('active');
-    // myButton.addEventListener('mouseout', removeActive)
     myBox.classList.add('rubberBand');
-
     myBox.addEventListener("animationend", removeRubber); 
-   
     myContent.appendChild(newBox);
     const newElement = document.createTextNode(myEmoti[Math.round(Math.random()*(myEmoti.length-1))]);
     newBox.appendChild(newElement);
-    newBox.style.visibility = "hidden";
     
     newBox.classList.add('box2', 'box2-animation' ); //, 'box2-animation' bounceInDown
     newBox.setAttribute("id","box4");
@@ -34,13 +29,9 @@ const breakeJump = function (){
 const removeRubber = function (){
     const mBox = document.getElementById('mbox');
     mBox.classList.remove('rubberBand');
-    document.getElementById('box4').style.visibility = 'visible';
 }
 
-const removeActive = function() {
-    const myButton = document.getElementById('rbutton');
-    myButton.classList.remove('active');
-}
+
 
 shot();
 
